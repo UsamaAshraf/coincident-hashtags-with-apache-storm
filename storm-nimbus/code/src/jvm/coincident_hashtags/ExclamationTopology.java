ab4311937b6b4bab014851e348ccc3482b39b6e1
Usama Ashraf <uashraf588@gmail.com>
2018-05-01 19:11:36 +0500

    Set topology workers to 8 - which will be divided among the supervisors

--- storm-nimbus/code/src/jvm/coincident_hashtags/ExclamationTopology.java
+++ storm-nimbus/code/src/jvm/coincident_hashtags/ExclamationTopology.java
@@ -81,7 +81,10 @@ public class ExclamationTopology {
       // run it in a live cluster
 
       // set the number of workers for running all spout and bolt tasks
-      conf.setNumWorkers(3);
+      // If we have two supervisors with 4 allocated workers each, and this topology is
+      // submitted to the master (Nimbus) node, then these 8 workers will be distributed
+      // to the two supervisors evenly: four each. 
+      conf.setNumWorkers(8);
 
       // create the topology and submit with config
       StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
