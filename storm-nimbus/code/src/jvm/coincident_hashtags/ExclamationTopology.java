58220715a1f6228c945fcbb137a413e76c9894cc
Usama Ashraf <uashraf588@gmail.com>
2018-05-10 18:10:06 +0500

    WIP

--- storm-nimbus/code/src/jvm/coincident_hashtags/ExclamationTopology.java
+++ storm-nimbus/code/src/jvm/coincident_hashtags/ExclamationTopology.java
@@ -1,7 +1,6 @@
 package coincident_hashtags;
 
 import org.apache.storm.Config;
-import org.apache.storm.LocalCluster;
 import org.apache.storm.StormSubmitter;
 import org.apache.storm.task.OutputCollector;
 import org.apache.storm.task.TopologyContext;
@@ -12,7 +11,6 @@ import org.apache.storm.topology.base.BaseRichBolt;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
-import org.apache.storm.utils.Utils;
 
 import java.util.Map;
 
@@ -58,55 +56,34 @@ public class ExclamationTopology {
 
   public static void main(String[] args) throws Exception
   {
-    // create the topology
     TopologyBuilder builder = new TopologyBuilder();
 
-    // attach the word spout to the topology - parallelism of 10
     builder.setSpout("word", new TestWordSpout(), 10);
 
-    // attach the exclamation bolt to the topology - parallelism of 3
     builder.setBolt("exclaim1", new ExclamationBolt(), 3).shuffleGrouping("word");
 
-    // attach another exclamation bolt to the topology - parallelism of 2
     builder.setBolt("exclaim2", new ExclamationBolt(), 2).shuffleGrouping("exclaim1");
 
     // create the default config object
     Config conf = new Config();
 
-    // set the config in debugging mode
+    // Turn on  debugging mode
     conf.setDebug(true);
+    
+    // Set the number of workers for running all spout and bolt tasks.
+    // If we have two supervisors with 4 allocated workers each, and this topology is
+    // submitted to the master (Nimbus) node, then these 8 workers will be distributed
+    // to the two supervisors evenly: four each. 
+    conf.setNumWorkers(3);
 
     if (args != null && args.length > 0) {
-
-      // run it in a live cluster
-
-      // set the number of workers for running all spout and bolt tasks
-      // If we have two supervisors with 4 allocated workers each, and this topology is
-      // submitted to the master (Nimbus) node, then these 8 workers will be distributed
-      // to the two supervisors evenly: four each. 
-      conf.setNumWorkers(8);
 
       // create the topology and submit with config
       StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
 
     } else {
 
-      // run it in a simulated local cluster
-
-      // create the local cluster instance
-      LocalCluster cluster = new LocalCluster();
-
-      // submit the topology to the local cluster
-      cluster.submitTopology("exclamation", conf, builder.createTopology());
-
-      // let the topology run for 20 seconds. note topologies never terminate!
-      Thread.sleep(20000);
-
-      // kill the topology
-      cluster.killTopology("exclamation");
-
-      // we are done, so shutdown the local cluster
-      cluster.shutdown();
+      StormSubmitter.submitTopology("exclamation-topology", conf, builder.createTopology());
     }
   }
 }
